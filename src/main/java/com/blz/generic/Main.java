d70e30ba7536977746101bee94c9b1bb6d0ed7bd
nayab jahan <nayabzafir@gmail.com>
2022-08-22 20:40:35 +0530

    Added:Maximum of three string using generic

--- src/main/java/com/blz/generic/Main.java
+++ src/main/java/com/blz/generic/Main.java
@@ -8,6 +8,8 @@ public class Main {
         System.out.printf("Maximum of %d, %d and %d is %d\n\n", 3, 4, 5, maximum(3, 4, 5));
         System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.6, 8.8, 7.7, maximum(6.6,
                 8.8, 7.7));
+        System.out.printf("Maximum of %s, %s and %s is %s\n", "apple", "peach", "banana", maximum(
+                "apple", "peach", "banana"));
     }
 
 }
