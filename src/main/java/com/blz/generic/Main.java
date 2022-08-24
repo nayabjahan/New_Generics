2ae4dbadbc185e2f9bf3b3e562fccf063dafda65
nayab jahan <nayabzafir@gmail.com>
2022-08-25 01:59:07 +0530

    Refactored:using generics method finding maximum

--- src/main/java/com/blz/generic/Main.java
+++ src/main/java/com/blz/generic/Main.java
@@ -4,13 +4,17 @@ import static com.blz.generic.IntMax.maximum;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Generics example");
-        System.out.printf("Maximum of %d, %d and %d is %d\n\n", 3, 4, 5, maximum(3, 4, 5));
-        System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.6, 8.8, 7.7, maximum(6.6,
-                8.8, 7.7));
-        System.out.printf("Maximum of %s, %s and %s is %s\n", "apple", "peach", "banana", maximum(
-                "apple", "peach", "banana"));
+        MaxGeneric<Integer> mainFirst = new MaxGeneric<>(9, 3, 6);
+        MaxGeneric<Float> mainSecond = new MaxGeneric<>(9.4f, 3.5f, 9.9f);
+        MaxGeneric<String> mainThird = new MaxGeneric<>("Apple", "Peach", "Banana");
+        Integer maxInt = mainFirst.testMaximum();
+        Float maxFloat = mainSecond.testMaximum();
+        String maxString = mainThird.testMaximum();
+        System.out.println("maximum integer is: " + maxInt);
+        System.out.println("maximum float is: " + maxFloat);
+        System.out.println("maximum String is: " + maxString);
     }
-
 }
+
+
 
